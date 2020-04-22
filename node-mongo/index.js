72fac8efb0c38c79db2d16d0d2c40a0130cff233
Divyangi <divyangisingh@outlook.com>
2020-04-22 17:16:09 +0530

    mongodb example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations')
 
 const url ='mongodb://localhost:27017/'
 const dbname = 'confusion';
@@ -8,23 +9,32 @@ MongoClient.connect(url,(err,client)=>{
     assert.equal(err, null);
     console.log('Connected correnctly to server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    collection.insertOne({"name":"Utthappizza","description":"Test"},(err,result) =>{
-        assert.equal(err,  null);
+    
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        console.log('After insert:\n')
-        console.log(result.ops);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
-            console.log('Found\n')
-        console.log(docs);
-        db.dropCollection('dishes',(err, result) => {
-            assert.equal(err, null);
-            client.close();
-        })
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-        })
-    })
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+                            
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
+
+                                client.close();
+                            });
+                        });
+                    });
+            });
+    });
+    
+   
 })
 //nesting of call one inside other
